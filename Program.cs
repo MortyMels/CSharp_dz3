fa4db53d77ca49247d80f462a911f77e4cf43bf0
MortyMels <Morty.Mels@gmail.com>
2022-09-02 09:13:11 +0300

    dz03.01.001

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool r = true; //Длинна вектора dz3
+Console.Clear();
+    
+while(r){
+    Console.WriteLine("Введите несколько наборов из трех чисел в формате '0.0, 0.0;'");
+    string s = Console.ReadLine().Replace(" ", "");
+    string[] sa = s.Split(';'); //разбили на точки
+    double[] cl;
+    foreach (var san in sa)
+    {
+        cl=StrToIntAr(san,true);
+
+        if((cl.Length-1==2)){
+            if (cl[0]==0){
+                Console.WriteLine($"Число ({cl[1]} в степени {cl[2]} = {Math.Pow(cl[1],cl[2])}");
+            }
+        }else{
+            Console.WriteLine($"Неверное количество параметров в наборе чисел: {san}");
+        }
+    }
+    if (s=="end"){
+        r=false;
+    }
+    
+}
+
+double[] StrToIntAr(string str, bool err) {
+    string[] stra = str.Split(',');
+    double[] numa = new double [stra.Length+1];
+    int i = 0;
+    foreach (string strai in stra){
+        if(!double.TryParse(strai, out numa[++i])){
+            if(err){
+                Console.WriteLine($"Нераспознан {i} элемента набора '{strai}' в: {str}");
+            }
+            numa[0]++;
+        }
+    }
+    return numa;
+}
